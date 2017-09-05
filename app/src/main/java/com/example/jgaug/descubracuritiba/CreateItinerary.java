235efad87afb7876538f7ef455ce62eff803f62b
José Guilherme <jose56_7@hotmail.com>
2017-09-04 21:54:05 -0300

    Adicionada opção de selecionar locais para visita.

--- app/src/main/java/com/example/jgaug/descubracuritiba/CreateItinerary.java
+++ app/src/main/java/com/example/jgaug/descubracuritiba/CreateItinerary.java
@@ -4,18 +4,17 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import java.util.Calendar;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.DatePicker;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class CreateItinerary extends AppCompatActivity {
 
@@ -29,210 +28,169 @@ public class CreateItinerary extends AppCompatActivity {
     private int year, month, day;
     private TextView dateView2;
     private int year2, month2, day2;
+    private boolean parksSelected = false;
+    private boolean landmarksSelected = false;
+    private boolean museumsSelected = false;
+    private boolean shoppingSelected = false;
+    private boolean foodsSelected = false;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_create_itinerary );
 
-        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString("parques", "Parques não");
-        editor.putString("museus", "Museus não");
-        editor.putString("locais historicos", "Locais históricos não");
-        editor.putString("pracas", "Praças não");
-        editor.putString("restaurantes", "Restaurantes não");
-        editor.apply();
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences( getApplicationContext( ) );
+        SharedPreferences.Editor editor = settings.edit( );
+        editor.putString( "parques", "Parques não" );
+        editor.putString( "museus", "Museus não" );
+        editor.putString( "locais historicos", "Locais históricos não" );
+        editor.putString( "pracas", "Praças não" );
+        editor.putString( "restaurantes", "Restaurantes não" );
+        editor.apply( );
 
-        dateView = (TextView) findViewById(R.id.textView13);
+        dateView = ( TextView ) findViewById( R.id.textView13 );
         //day = 22;
         //month = 07;
         //year = 2017;
-        calendar = Calendar.getInstance();
-        year = calendar.get(Calendar.YEAR);
-        month = calendar.get(Calendar.MONTH);
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-        showDate1(year, month+1, day);
+        calendar = Calendar.getInstance( );
+        year = calendar.get( Calendar.YEAR );
+        month = calendar.get( Calendar.MONTH );
+        day = calendar.get( Calendar.DAY_OF_MONTH );
+        showDate1( year, month + 1, day );
 
-        editor.putInt("year1",year);
-        editor.putInt("month1",month);
-        editor.putInt("day1",day);
-        editor.apply();
+        editor.putInt( "year1", year );
+        editor.putInt( "month1", month );
+        editor.putInt( "day1", day );
+        editor.apply( );
 
-        dateView2 = (TextView) findViewById(R.id.textView14);
+        dateView2 = ( TextView ) findViewById( R.id.textView14 );
         //day2 = 22;
         //month2 = 07;
         //year2 = 2017;
-        calendar2 = Calendar.getInstance();
-        year2 = calendar2.get(Calendar.YEAR);
-        month2 = calendar2.get(Calendar.MONTH);
-        day2 = calendar2.get(Calendar.DAY_OF_MONTH);
-        showDate2(year2, month2+1, day2);
-        editor.putInt("year2",year2);
-        editor.putInt("month2",month2);
-        editor.putInt("day2",day2);
-        editor.apply();
+        calendar2 = Calendar.getInstance( );
+        year2 = calendar2.get( Calendar.YEAR );
+        month2 = calendar2.get( Calendar.MONTH );
+        day2 = calendar2.get( Calendar.DAY_OF_MONTH );
+        showDate2( year2, month2 + 1, day2 );
+        editor.putInt( "year2", year2 );
+        editor.putInt( "month2", month2 );
+        editor.putInt( "day2", day2 );
+        editor.apply( );
 
-        TextView tv1=(TextView) findViewById(R.id.textView15);
-        tv1.setText("00 : 00");
+        TextView tv1 = ( TextView ) findViewById( R.id.textView15 );
+        tv1.setText( "00 : 00" );
 
-        TextView tv2=(TextView) findViewById(R.id.textView16);
-        tv2.setText("00 : 00");
+        TextView tv2 = ( TextView ) findViewById( R.id.textView16 );
+        tv2.setText( "00 : 00" );
     }
 
-    public void showTimePickerDialog(View v) {
-        DialogFragment newFragment = new TimePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "timePicker");
-    }
-
-    public void showTimePickerDialog2(View v) {
-        DialogFragment newFragment2 = new TimePickerFragment2();
-        newFragment2.show(getSupportFragmentManager(), "timePicker");
-    }
-
-    public void onCheckboxClicked(View view){
-        boolean checked = ((CheckBox) view).isChecked();
-
-        switch(view.getId()){
-            case R.id.checkBox_parques:
-                if(checked){
-                    SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    SharedPreferences.Editor editor = settings.edit();
-                    editor.putString("parques", "Parques sim");
-                    editor.apply();
-                    Toast.makeText( this, "Você selecionou parques", Toast.LENGTH_SHORT ).show( );
-                }
-                else{
-                    Toast.makeText( this, "Você tirou parques", Toast.LENGTH_SHORT ).show( );
-                }
+    public void onSelectPlacesToVisit( View view ) {
+        switch( view.getId( ) ) {
+            case R.id.imageViewParks:
+                parksSelected = !parksSelected;
+                changeImageViewResource( parksSelected, R.id.imageViewParks, R.drawable.parks_checked, R.drawable.parks_unchecked );
                 break;
-            case R.id.checkBox_museus:
-                if(checked){
-                    SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    SharedPreferences.Editor editor = settings.edit();
-                    editor.putString("museus", "Museus sim");
-                    editor.apply();
-                    Toast.makeText( this, "Você selecionou museus", Toast.LENGTH_SHORT ).show( );
-                }
-                else{
-                    Toast.makeText( this, "Você tirou museus", Toast.LENGTH_SHORT ).show( );
-                }
+            case R.id.imageViewLandmarks:
+                landmarksSelected = !landmarksSelected;
+                changeImageViewResource( landmarksSelected, R.id.imageViewLandmarks, R.drawable.landmarks_checked, R.drawable.landmarks_unchecked );
                 break;
-            case R.id.checkBox_locais_hist:
-                if(checked){
-                    SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    SharedPreferences.Editor editor = settings.edit();
-                    editor.putString("locais historicos", "Locais históricos sim");
-                    editor.apply();
-                    Toast.makeText( this, "Você selecionou locais históricos", Toast.LENGTH_SHORT ).show( );
-                }
-                else{
-                    Toast.makeText( this, "Você tirou locais históricos", Toast.LENGTH_SHORT ).show( );
-                }
+            case R.id.imageViewMuseums:
+                museumsSelected = !museumsSelected;
+                changeImageViewResource( museumsSelected, R.id.imageViewMuseums, R.drawable.museums_checked, R.drawable.museums_unchecked );
                 break;
-            case R.id.checkBox_pracas:
-                if(checked){
-                    SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    SharedPreferences.Editor editor = settings.edit();
-                    editor.putString("pracas", "Locais de compra sim");
-                    editor.apply();
-                    Toast.makeText( this, "Você selecionou locais de compra", Toast.LENGTH_SHORT ).show( );
-                }
-                else{
-                    Toast.makeText( this, "Você tirou locais de compra", Toast.LENGTH_SHORT ).show( );
-                }
+            case R.id.imageViewShopping:
+                shoppingSelected = !shoppingSelected;
+                changeImageViewResource( shoppingSelected, R.id.imageViewShopping, R.drawable.shopping_checked, R.drawable.shopping_unchecked );
                 break;
-            case R.id.checkBox_restaurantes:
-                if(checked){
-                    SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    SharedPreferences.Editor editor = settings.edit();
-                    editor.putString("restaurantes", "Restaurantes sim");
-                    editor.apply();
-                    Toast.makeText( this, "Você selecionou restaurantes", Toast.LENGTH_SHORT ).show( );
-                }
-                else{
-                    Toast.makeText( this, "Você tirou restaurantes", Toast.LENGTH_SHORT ).show( );
-                }
+            case R.id.imageViewFood:
+                foodsSelected = !foodsSelected;
+                changeImageViewResource( foodsSelected, R.id.imageViewFood, R.drawable.food_checked, R.drawable.food_unchecked );
                 break;
         }
     }
 
-    @SuppressWarnings("deprecation")
-    public void setDate(View view) {
-        showDialog(999);
-        Toast.makeText(getApplicationContext(), "ca",
-                Toast.LENGTH_SHORT)
-                .show();
+    private void changeImageViewResource( boolean placeSelected, int imageViewId, int checkedImageViewId, int uncheckedImageViewId ) {
+        ImageView imageView = ( ImageView ) findViewById( imageViewId );
+        if( placeSelected == true ) {
+            imageView.setImageResource( checkedImageViewId );
+        } else {
+            imageView.setImageResource( uncheckedImageViewId );
+        }
     }
 
-    @SuppressWarnings("deprecation")
-    public void setDate2(View view) {
-        showDialog(998);
-        Toast.makeText(getApplicationContext(), "ca",
-                Toast.LENGTH_SHORT)
-                .show();
+    public void showTimePickerDialog( View v ) {
+        DialogFragment newFragment = new TimePickerFragment( );
+        newFragment.show( getSupportFragmentManager( ), "timePicker" );
+    }
+
+    public void showTimePickerDialog2( View v ) {
+        DialogFragment newFragment2 = new TimePickerFragment2( );
+        newFragment2.show( getSupportFragmentManager( ), "timePicker" );
+    }
+
+    @SuppressWarnings( "deprecation" )
+    public void setDate( View view ) {
+        showDialog( 999 );
+        Toast.makeText( getApplicationContext( ), "ca", Toast.LENGTH_SHORT ).show( );
+    }
+
+    @SuppressWarnings( "deprecation" )
+    public void setDate2( View view ) {
+        showDialog( 998 );
+        Toast.makeText( getApplicationContext( ), "ca", Toast.LENGTH_SHORT ).show( );
     }
 
     @Override
-    protected Dialog onCreateDialog(int id) {
+    protected Dialog onCreateDialog( int id ) {
         // TODO Auto-generated method stub
-        if (id == 999) {
-            return new DatePickerDialog(this,
-                    myDateListener, year, month, day);
-        }
-        else if(id == 998){
-            return new DatePickerDialog(this,
-                    myDateListener2, year2, month2, day2);
+        if( id == 999 ) {
+            return new DatePickerDialog( this, myDateListener, year, month, day );
+        } else if( id == 998 ) {
+            return new DatePickerDialog( this, myDateListener2, year2, month2, day2 );
         }
         return null;
     }
 
-    private DatePickerDialog.OnDateSetListener myDateListener = new
-            DatePickerDialog.OnDateSetListener() {
-                @Override
-                public void onDateSet(DatePicker arg0,
-                                      int arg1, int arg2, int arg3) {
-                    // TODO Auto-generated method stub
-                    // arg1 = year
-                    // arg2 = month
-                    // arg3 = day
-                    showDate1(arg1, arg2+1, arg3);
-                }
-            };
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener( ) {
+        @Override
+        public void onDateSet( DatePicker arg0, int arg1, int arg2, int arg3 ) {
+            // TODO Auto-generated method stub
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            showDate1( arg1, arg2 + 1, arg3 );
+        }
+    };
 
-    private DatePickerDialog.OnDateSetListener myDateListener2 = new
-            DatePickerDialog.OnDateSetListener() {
-                @Override
-                public void onDateSet(DatePicker arg0,
-                                      int arg1, int arg2, int arg3) {
-                    // TODO Auto-generated method stub
-                    // arg1 = year
-                    // arg2 = month
-                    // arg3 = day
-                    showDate2(arg1, arg2+1, arg3);
-                }
-            };
+    private DatePickerDialog.OnDateSetListener myDateListener2 = new DatePickerDialog.OnDateSetListener( ) {
+        @Override
+        public void onDateSet( DatePicker arg0, int arg1, int arg2, int arg3 ) {
+            // TODO Auto-generated method stub
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            showDate2( arg1, arg2 + 1, arg3 );
+        }
+    };
 
-    private void showDate1(int year, int month, int day) {
-        dateView.setText(new StringBuilder().append(day).append("/")
-                .append(month).append("/").append(year));
-        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putInt("year1",year);
-        editor.putInt("month1",month);
-        editor.putInt("day1",day);
-        editor.commit();
+    private void showDate1( int year, int month, int day ) {
+        dateView.setText( new StringBuilder( ).append( day ).append( "/" ).append( month ).append( "/" ).append( year ) );
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences( getApplicationContext( ) );
+        SharedPreferences.Editor editor = settings.edit( );
+        editor.putInt( "year1", year );
+        editor.putInt( "month1", month );
+        editor.putInt( "day1", day );
+        editor.commit( );
     }
 
-    private void showDate2(int year, int month, int day) {
-        dateView2.setText(new StringBuilder().append(day).append("/")
-                .append(month).append("/").append(year));
-        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putInt("year2",year);
-        editor.putInt("month2",month);
-        editor.putInt("day2",day);
-        editor.commit();
+    private void showDate2( int year, int month, int day ) {
+        dateView2.setText( new StringBuilder( ).append( day ).append( "/" ).append( month ).append( "/" ).append( year ) );
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences( getApplicationContext( ) );
+        SharedPreferences.Editor editor = settings.edit( );
+        editor.putInt( "year2", year );
+        editor.putInt( "month2", month );
+        editor.putInt( "day2", day );
+        editor.commit( );
     }
 
     public void btnMakeItinerary( View view ) {
